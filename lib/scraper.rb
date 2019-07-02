2cae05ba48fbbb71b9b485a79c6dbe81e0015396
Lukie Kang <lukiek+f@gmail.com>
2019-07-02 17:06:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,8 +13,11 @@ class Scraper
     #   course.schedule = post.css(".date").text
     #   course.description = post.css("p").text
     # end
-
   end # of get_page
+
+  def get_courses
+    self.get_page.css(".post")
+  end
 
 Scraper.new.get_page
 
