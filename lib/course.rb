db31ea5148c3925a397da9a36b97fa51d0a50433
Lukie Kang <lukiek+f@gmail.com>
2019-07-02 16:49:04 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,20 @@
 class Course
   attr_accessor :title, :schedule, :description
+
+@@all = []
+
+def initialize
+  @@all << self
+end
+
+def self.all
+end
+
+def reset_all
+end
+
+
+
+
+  
 end # of class
